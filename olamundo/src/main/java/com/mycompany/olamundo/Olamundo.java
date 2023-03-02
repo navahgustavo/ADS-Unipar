990cb2f8d345a30cafb81afee5ebec631dbc1f56
Gustavo H. Nava <navahgustavo@gmail.com>
2023-03-02 19:30:20 -0300

    Add files via upload

---
+++ olamundo/src/main/java/com/mycompany/olamundo/Olamundo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.olamundo;
+
+/**
+ *
+ * @author aluno
+ */
+public class Olamundo {
+
+    public static void main(String[] args) {
+        if (10 > 20){
+            System.out.println("Olá mundo!");
+        } else {
+            System.out.println("Olá Unipar!");
+        }
+    }
+}
