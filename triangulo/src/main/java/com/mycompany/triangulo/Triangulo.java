0915ef3d81dee3be7ef143608b7afabf030556c2
Gustavo H. Nava <navahgustavo@gmail.com>
2023-03-02 20:07:59 -0300

    Add files via upload

--- triangulo/src/main/java/com/mycompany/triangulo/Triangulo.java
+++ triangulo/src/main/java/com/mycompany/triangulo/Triangulo.java
@@ -5,16 +5,18 @@ import java.util.Scanner;
 public class Triangulo {
 
     public static void main(String[] args) {
-        int base, altura, area;
+        int base, altura;
+        double area;
         Scanner ler = new Scanner(System.in);
         
-        System.out.println("Informe o valor da base: ");
+        System.out.println("Informe o valor da base em cm: ");
         base = ler.nextInt();
-        System.out.println("Informe o valor da altura: ");
+        System.out.println("Informe o valor da altura em cm: ");
         altura = ler.nextInt();
         
         area = (base*altura)/2;
         
-        System.out.println("A área do triângulo é: "+area);
+        System.out.println("A área do triângulo em cm²: "+area);
+        System.out.println("A área do triângulo em m²: "+area*0.01);
     }
 }
